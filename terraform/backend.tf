95e7fadb34e84611373674d7cd20d4aaa4011864
Mohamed Khalil <mohammdahmedkhalil@gmail.com>
2026-08-20 10:37:10 +0300

    Update S3 bucket name for Terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-s3"
+    bucket         = "my-tf-s3bucket-2026"
     key            = "ec2-project/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
