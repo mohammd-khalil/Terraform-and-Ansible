0bf1910034ffa4669e2355c18407dba3a942e07e
mohammd-khalil <mohammdahmedkhalil@gmail.com>
2026-08-25 05:00:52 +0300

    Replace the port number

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -12,8 +12,8 @@ resource "aws_security_group" "ssh_access" {
   }
   ingress {
     description = "Inbound traffic allowed from anyone on the internet"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
